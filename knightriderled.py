fe09cbff78fac2354c6d3d600db8fc086376d223
TheRock77 <68679582+TheRock77@users.noreply.github.com>
2021-06-22 22:45:05 +0200

    Update knightriderled.py
    fix some output stuff and formating

--- knightriderled.py
+++ knightriderled.py
@@ -2,7 +2,7 @@
 import RPi.GPIO as GPIO 
 import time 
 
-print("Hello Knight-Rider") 
+print("Willkommen zu Knight-Rider LED")
 
 #AUFGABE
 ledPins = [7,8,9,10,11] # AUFGABE: lass LED 9 bis 13 blinken
@@ -11,16 +11,12 @@ ledPins = [7,8,9,10,11] # AUFGABE: lass LED 9 bis 13 blinken
 
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme 
 GPIO.setup(ledPins, GPIO.OUT) # Definiere die ledPins als output pins 
-
-print("Willkommen zu Knight-Rider LED")
-print("Druecke STRG+C um das Programm zu beenden!") 
-
 #AUFGABE: schalte LEDs 9 bis 13 ab
 # GPIO.output( )
 
-try: # Weiss nicht ob Try-Catch nicht den Rahmen bissl sprengt 
-    
-    while 1:     
+print("Druecke STRG+C um das Programm zu beenden!") 
+try:
+    while True:     
         for x in ledPins:
             # AUFGABE: bringe LEDs 9 bis 13 wie bei Knight-Rider zum leuchten
             time.sleep(0.50)
