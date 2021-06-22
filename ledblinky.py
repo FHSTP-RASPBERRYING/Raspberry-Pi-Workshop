7d94255d0c6dfeb05aae5719a0a1409035742868
TheRock77 <68679582+TheRock77@users.noreply.github.com>
2021-06-22 22:46:16 +0200

    Update ledblinky.py
    fix some formating stuff

--- ledblinky.py
+++ ledblinky.py
@@ -9,18 +9,15 @@ print("Hallo zu LED Blinky!")
 ledPin = 11 # Definiere ledPin (PIN 9) als Output Pin!
 
 # Pin Setup: 
-
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme 
 GPIO.setup(ledPin, GPIO.OUT) # Definiere ledPin (PIN 9) als Output Pin! 
 
 print("Druecke STRG+C um das Programm zu beenden!") 
 
 try:
-    
-    while 1: 
+    while True: 
         #AUFGABE
         time.sleep(2)
         
 except KeyboardInterrupt: # Wenn STRG+C gedrueckt wird beende programm 
-
     GPIO.cleanup() # GPIO Pins freigeben 
